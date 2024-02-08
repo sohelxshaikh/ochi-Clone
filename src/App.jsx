cd9b641259b6adedf26ac635fc008c1968aca56e
sohelxshaikh <sohailhacker7786@gmail.com>
2024-02-08 18:58:27 +0530

    Eyes PAge

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Navbar from './components/Navbar';
 import Landing from './components/Landing';
 import Marqee from './components/Marqee';
 import About from './components/About';
+import Eyes from './components/Eyes';
 
 function App() {
   return (
-    <div className='w-full min-h-screen bg-zinc-900 text-white'>
+    <div className='w-full min-h-screen bg-zinc-900 overflow-hidden text-white'>
       <Navbar/>
       <Landing/>
       <Marqee/>
       <About/>
+      <Eyes/>
     </div>
   )
 }
---
+++ src/components/Eyes.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Eyes() {
+  return (
+    <div className='eyes w-full h-screen overflow-hidden'>
+      <div className='relative w-full h-full bg-cover bg-center bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")]'>
+        <div className='absolute gap-16 items-center flex justify-between top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'>
+          <div className='bg-white flex items-center justify-center w-[15vw] h-[15vw] rounded-full'>
+            <div className='bg-[#252525] relative w-[10vw] h-[10vw] rounded-full'>
+            <div className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full'>
+                <div className='bg-white w-[2vw] h-[2vw] rounded-full'>
+
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='bg-white flex items-center justify-center w-[15vw] h-[15vw] rounded-full'>
+            <div className='bg-[#252525] relative w-[10vw] h-[10vw] rounded-full'>
+            <div className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full'>
+                <div className='bg-white w-[2vw] h-[2vw] rounded-full'>
+
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Eyes
